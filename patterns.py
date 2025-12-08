94039f4115811a5926127fa992c1f059178c7813
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-08 13:34:39 +0530

    patters and Link

---
+++ patterns.py
@@ -0,0 +1,12 @@
+n=5#takes up the colums
+for rows in range(1,n+1):# n number of rows and colums
+  for cols in range(1,n+1):#loop n-rows -> rremoving 1 elemets at once
+    print("*",end=" ")
+  print()
+#* * * * *  - solution->o/p
+#* * * * * 
+#* * * * * 
+#* * * * * 
+#* * * * * 
+# find the reamining patterns in patterns.py
+# link : https://colab.research.google.com/drive/1BddWCT9_7yUxU11HbHxsqfIDo73SIqCz?usp=sharing
