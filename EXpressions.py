e771b7046c6b23e00d9831c3e3187fff8de5c8d6
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-10 09:25:52 +0530

    Expressions

---
+++ EXpressions.py
@@ -0,0 +1,9 @@
+import re
+#code for regular expressions to extract email addresses from a string
+'''text = "Emails:test@example.com,hello.world@company.org"
+# Regular expression to match email addresses
+email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
+# Find all email addresses in the text
+emails = re.findall(email_pattern, text)
+# Print the list of email addresses
+print("Extracted emails:", emails)'''      
