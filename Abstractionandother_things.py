9025f8c8e23dd5c2a33317596aaf806659443b4a
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-10 14:46:37 +0530

    final - oop cconcept

--- Abstractionandother_things.py
+++ Abstractionandother_things.py
@@ -15,3 +15,6 @@ c1.drive()
 c1.run()
 #Class with multiple constructor methods and a best example for abstraction in python
 # Remaining check google colab link .
+#remaining code is in google colab
+# https://colab.research.google.com/drive/1WQY7YkV1b2YJ3Z8Y6I9KXG6k1Z7b8F0?usp=sharing      
+#check there for the encapsulation and in heritence code  - visit it 
