9d756f138e247316df14952a8a348d539837888a
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-10 10:26:30 +0530

    Object Oriented Programming - syntax and Constructor methiod

---
+++ oop.py
@@ -0,0 +1,19 @@
+#Class and Object Representation in Python
+'''
+class Student():
+    name = "Prince"
+    age = 20
+s1 = Student()
+
+print("Name:", s1.name)
+print("Age:", s1.age)   
+
+'''    
+#Contructor Method
+'''class Laptop():
+    def __init__(self):
+        print("Laptop constructor ")
+    def show(self):
+        print("Display")
+obj = Laptop()
+print(obj.show())'''
