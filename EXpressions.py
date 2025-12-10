4bd05e91f692a6c3c695d85887edf57bfd38d009
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-10 09:36:01 +0530

    matching strs using reg.expressions

--- EXpressions.py
+++ EXpressions.py
@@ -6,4 +6,15 @@ email_pattern = r'[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}'
 # Find all email addresses in the text
 emails = re.findall(email_pattern, text)
 # Print the list of email addresses
-print("Extracted emails:", emails)'''      
+print("Extracted emails:", emails)'''
+# matching the two strings in regular expressions
+'''     
+text1 = "Hello Python"
+texxt2 = "programming Hello World"
+pattern = r"Hello" 
+match1 = re.match(pattern, text1)
+print(f"Match1{text1}: {match1.group() if match1 else "None"}")
+match2 = re.match(pattern, texxt2)
+print(f"Match2{texxt2}: {match2.group() if match2 else "None"}")
+# Using re.search to find the first occurrence of "Hello" in the string
+'''
