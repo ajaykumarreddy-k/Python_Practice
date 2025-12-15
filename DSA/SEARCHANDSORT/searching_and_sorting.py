6fd5874b13735746ed19d19c46397fcaead919af
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-15 14:04:14 +0530

    rename and selection sort

--- DSA/SEARCHANDSORT/searching_and_sorting.py
+++ DSA/SEARCHANDSORT/searching_and_sorting.py
@@ -34,3 +34,31 @@ def binary_search(arr, target):
 arr_for_binary_search = [1,2,3,423,23,2,3,23,23,23,7]
 arr_for_binary_search.sort()
 print(binary_search(arr_for_binary_search, 7))
+
+
+# Bubble Sort
+my_list = [64, 34, 25, 12, 22, 11, 90]
+n = len(my_list)
+for i in range(n):
+    for j in range(0, n-i-1):
+        if my_list[j] > my_list[j+1]:
+            my_list[j], my_list[j+1] = my_list[j+1], my_list[j]  # Swap if the element found is greater
+print("Sorted array is:", my_list)  # Output: Sorted array is: [11, 12, 22, 25, 34, 64, 90]
+#time complexity: O(n^2) - for worst case
+#space complexity: O(1) - no additional space used
+sorted_array1 = sorted(my_list)  # Using built-in sort function
+print(sorted_array1)  # Output: [11, 12, 22, 25, 34, 64, 90]
+
+# Selection Sort
+def selection_sort(arr):
+    n = len(arr)
+    # Traverse through all array elements
+    for i in range(n):
+        # Find the minimum element in remaining unsorted array
+        min_idx = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        # Swap the found minimum element with the first element
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
