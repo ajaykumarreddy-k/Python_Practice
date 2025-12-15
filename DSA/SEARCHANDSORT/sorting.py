06c32552dbd487c2c48484d03967ca911630db8b
ajaykumarreddy-k <ajaykumarreddykrishnareddygari@gmail.com>
2025-12-15 11:36:30 +0530

    Binary search added

--- DSA/SEARCHANDSORT/sorting.py
+++ DSA/SEARCHANDSORT/sorting.py
@@ -13,3 +13,24 @@ def linear_search(arr, target):
             return i  # Return the index if target is found
     return -1  # Return -1 if target is not found
 print(linear_search([1,2,33,4,5,65,43,23,23,2,7], 7))  # Output: 6
+#time complexity: O(1) - when item is in the first position
+#time complexity: O(n) - when item is in the last position or not present
+#space complexity: O(1) - no additional space used
+
+# Example usage of linear search
+# binary Search
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1  # Initialize the left and right pointers
+
+    while left <= right:
+        mid = (left + right) // 2  # Find the middle index
+        if arr[mid] == target:
+            return mid  # Return the index if target is found
+        elif arr[mid] < target:
+            left = mid + 1  # Move the left pointer to the right
+        else:
+            right = mid - 1  # Move the right pointer to the left
+    return -1  # Return -1 if target is not found
+arr_for_binary_search = [1,2,3,423,23,2,3,23,23,23,7]
+arr_for_binary_search.sort()
+print(binary_search(arr_for_binary_search, 7))
